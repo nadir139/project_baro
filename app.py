afb14d16342646504386b4b2b68f65ba65f757d0
Nadir Balena <nadir.balena@gmail.com>
2023-12-13 15:37:52 +0100

    upload 404 550 error

--- app.py
+++ app.py
@@ -15,5 +15,15 @@ def post_comment():
     job_comments.append(comment_text)
     return redirect(url_for('index'))
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+  return render_template("404.html"), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+  return render_template("500.html"), 500
+
+
 if __name__ == '__main__':
     app.run(debug=True)
